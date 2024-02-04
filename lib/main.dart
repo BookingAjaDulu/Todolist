4267b2ee2af723c4f06bf9d40ecdf65502a67f87
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-02-04 17:36:36 +0700

    update tittle

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Aplikasi Flutter Pertama",
+      title: "Aplikasi Flutter Booking",
       home: LoginScreen(),
     );
   }
